d86aa32a201c4666d3a8f2769afc0b612922facb
MrFarhan <muhammadfarhan012345@gmail.com>
2021-09-26 20:46:43 +0500

    Changed api of covid-19

--- src/components/GlobalData.js
+++ src/components/GlobalData.js
@@ -1,81 +1,167 @@
-import React, { useState, useEffect } from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Typography from '@material-ui/core/Typography';
+import React, { useState, useEffect, useRef } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Typography from "@material-ui/core/Typography";
 
-import Paper from '@material-ui/core/Paper';
-import NumberFormat from 'react-number-format';
+import Paper from "@material-ui/core/Paper";
+import NumberFormat from "react-number-format";
+var axios = require("axios").default;
 
 const useStyles = makeStyles((theme) => ({
-    root: {
-        display: 'flex',
-        flexWrap: 'wrap',
-        '& > *': {
-            margin: theme.spacing(1),
-            width: '100%',
-            height: theme.spacing(16),
-        },
+  root: {
+    display: "flex",
+    flexWrap: "wrap",
+    "& > *": {
+      margin: theme.spacing(1),
+      width: "100%",
+      height: theme.spacing(16),
     },
+  },
 }));
 
-export default function GlobalData() {
-    const classes = useStyles();
-    const [globalData, setGlobalData] = useState();
-    const [loading, setLoading] = useState(false);
+export const GlobalData = () => {
+  const classes = useStyles();
+  const [country, setCountry] = useState("pk");
+  const [globalData, setGlobalData] = useState();
+  const [loading, setLoading] = useState(false);
+  const [err,setErr] = useState(false)
+  const apiRef = useRef();
 
-    useEffect(() => {
-        async function fetchGlobal() {
-            setLoading(true)
-            const apiRes = await fetch('https://api.thevirustracker.com/free-api?global=stats')
-            const jsonRes = await apiRes.json()
-            console.log(jsonRes, "data")
-            setGlobalData(jsonRes);
-            setLoading(false)
-            // console.log(total, "total ")
-        }
-        fetchGlobal()
-    }, [])
-    const Loading = "Loading..."
+  const fetchGlobal = () => {
+    var options = {
+      method: "GET",
+      url: process.env.REACT_APP_RAPID_URL,
+      params: { code: country },
+      headers: {
+        "x-rapidapi-host": process.env.REACT_APP_RAPID_HOST,
+        "x-rapidapi-key": process.env.REACT_APP_RAPID_API,
+      },
+    };
 
+    axios
+      .request(options)
+      .then(function (response) {
+        setGlobalData(response.data);
+      })
+      .catch(function (error) {
+        setErr(true)
+      });
+  };
+  apiRef.current = fetchGlobal;
+
+  useEffect(() => {
+    apiRef.current();
+  }, []);
+
+  const Loading = "Loading...";
+  console.log("gd", err);
     if (loading) {
-        return Loading
+      return Loading;
     } else
-        return (
-            <div className={classes.root}>
-
-                <Paper elevation={3} >
-                    <Typography variant="h4" gutterBottom>
-                    <NumberFormat value={globalData && globalData.results && globalData.results[0].total_cases} displayType={'text'} thousandSeparator={true} renderText={value => <div>{value}</div>} />
-                        
-                    </Typography>
-                    <Typography variant="subtitle2" gutterBottom style={{ fontWeight: 'bold' }}>
-                        Global data as of today
-                 </Typography>
-                </Paper>
-                <Paper elevation={3} >
-                    <Typography variant="h4" gutterBottom style={{ fontWeight: 'bold', color: 'orange' }}>
-                    <NumberFormat value={globalData && globalData.results && globalData.results[0].total_active_cases} displayType={'text'} thousandSeparator={true} renderText={value => <div>{value}</div>} />                    
-                </Typography>
-                    <Typography variant="subtitle2" gutterBottom style={{ fontWeight: 'bold', color: 'orange' }}>
-                        Total Active Cases
-                 </Typography>
-                </Paper>
-                <Paper elevation={3} >
-                    <Typography variant="h4" gutterBottom style={{ fontWeight: 'bold', color: 'green' }}>
-                    <NumberFormat value={globalData && globalData.results && globalData.results[0].total_recovered} displayType={'text'} thousandSeparator={true} renderText={value => <div>{value}</div>} />
-                </Typography>
-                    <Typography variant="subtitle2" gutterBottom style={{ fontWeight: 'bold', color: 'green' }}>
-                        Total Recovered Cases
-                 </Typography>
-                </Paper>
-                <Paper elevation={3} >
-                    <Typography variant="h4" gutterBottom style={{ fontWeight: 'bold', color: 'red' }}>
-                    <NumberFormat value={globalData && globalData.results && globalData.results[0].total_deaths} displayType={'text'} thousandSeparator={true} renderText={value => <div>{value}</div>} />
-                    
-                </Typography>
-                    <Typography variant="subtitle2" gutterBottom style={{ fontWeight: 'bold', color: 'red' }}>
-                        Total Deaths
-                 </Typography>
-                </Paper>
-            </div>
-        );
-}
+  return (
+    <div className={classes.root}>
+      <Paper elevation={3}>
+        <Typography variant="p" gutterBottom style={{ fontWeight: "bold", color: "#d8cdcd" }}> 
+          Input country code (i.e pk for Pakistan) for the country
+        </Typography>
+        <Typography
+          variant="subtitle2"
+          gutterBottom
+          style={{ fontWeight: "bold" }}
+        >
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              flexDirection: "column",
+              alignItems: "center",
+              padding: "1.5em",
+            }}
+          >
+            <label htmlFor="input">
+              {" "}
+              <input
+                type="text"
+                id="input"
+                onChange={(e) => setCountry(e?.target?.value)}
+                onClick={() =>  setErr(false)}
+              />
+              <button type="submit" onClick={fetchGlobal} disabled={err ? true : false}>
+                Search
+              </button>
+            </label>
+          </div>
+          <Typography
+            variant="subtitle2"
+            gutterBottom
+            style={{ fontWeight: "bold", color: "gray" }}
+          >
+            Currently showing results for {globalData?.length && globalData[0].country ? globalData[0].country: !globalData?.length ? "un-recognized country" : ""}
+          </Typography>
+        </Typography>
+      </Paper>
+      <Paper elevation={3}>
+        <Typography
+          variant="h4"
+          gutterBottom
+          style={{ fontWeight: "bold", color: "orange" }}
+        >
+          <NumberFormat
+            value={globalData?.length && globalData[0].confirmed}
+            displayType={"text"}
+            thousandSeparator={true}
+            renderText={(value) => <div>{value}</div>}
+          />
+        </Typography>
+        <Typography
+          variant="subtitle2"
+          gutterBottom
+          style={{ fontWeight: "bold", color: "orange" }}
+        >
+          Total Confirmed Cases
+        </Typography>
+      </Paper>
+      <Paper elevation={3}>
+        <Typography
+          variant="h4"
+          gutterBottom
+          style={{ fontWeight: "bold", color: "green" }}
+        >
+          <NumberFormat
+            value={globalData?.length && globalData[0].critical}
+            displayType={"text"}
+            thousandSeparator={true}
+            renderText={(value) => <div>{value}</div>}
+          />
+        </Typography>
+        <Typography
+          variant="subtitle2"
+          gutterBottom
+          style={{ fontWeight: "bold", color: "green" }}
+        >
+          Total Critical Cases
+        </Typography>
+      </Paper>
+      <Paper elevation={3}>
+        <Typography
+          variant="h4"
+          gutterBottom
+          style={{ fontWeight: "bold", color: "red" }}
+        >
+          <NumberFormat
+            value={globalData?.length  && globalData[0].deaths}
+            displayType={"text"}
+            thousandSeparator={true}
+            renderText={(value) => <div>{value}</div>}
+          />
+        </Typography>
+        <Typography
+          variant="subtitle2"
+          gutterBottom
+          style={{ fontWeight: "bold", color: "red" }}
+        >
+          Total Deaths
+        </Typography>
+      </Paper>
+    </div>
+  );
+};
--- src/components/mainGrid.js
+++ src/components/mainGrid.js
@@ -1,58 +1,36 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Paper from '@material-ui/core/Paper';
-import Grid from '@material-ui/core/Grid';
-import GlobalData from './GlobalData'
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Paper from "@material-ui/core/Paper";
+import Grid from "@material-ui/core/Grid";
+import { GlobalData } from "./GlobalData";
 
 const useStyles = makeStyles((theme) => ({
-    root: {
-        flexGrow: 1,
-    },
-    paper: {
-        padding: theme.spacing(2),
-        textAlign: 'center',
-        color: theme.palette.text.secondary,
-    },
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: "center",
+    color: theme.palette.text.secondary,
+  },
 }));
 
-
 export default function MainGrid() {
-    const classes = useStyles();
+  const classes = useStyles();
 
-    // async function GlobalCount() {
-    //     let response = await fetch(('https://api.thevirustracker.com/free-api?global=stats'))
-    //     let data = await response.json()
-    //     let total = await data.results[0].total_cases
-    //     console.log(data, "overall data")
-    //     console.log(total, "0 indexed data")
+  return (
+    <div className={classes.root}>
+      <Grid container spacing={3}>
+        <Grid item xs={4}>
+          <Paper className={classes.paper}>
+            <GlobalData />
+          </Paper>
+        </Grid>
 
-    // }
-
-    // // fetch('')
-    // GlobalCount();
-
-    return (
-        <div className={classes.root}>
-            <Grid container spacing={3}>
-
-                <Grid item xs={4}>
-                    <Paper className={classes.paper}>
-                        <GlobalData/>
-                    </Paper>
-                </Grid>
-
-
-                <Grid item xs={8}>
-                    <Paper className={classes.paper}>Local Count</Paper>
-                </Grid>
-
-            </Grid>
-        </div>
-    );
+        <Grid item xs={8}>
+          <Paper className={classes.paper}>Local Count Coming soon</Paper>
+        </Grid>
+      </Grid>
+    </div>
+  );
 }
-
-
-// <Paper className={classes.paper} style={{backgroundColor:'yellow'}}>Global Count
-//         <Paper className={classes.paper} style={{backgroundColor:'red'}}>Active Cases</Paper>
-//         <Paper className={classes.paper} style={{backgroundColor:'green'}}>Recovered</Paper>
-//         <Paper className={classes.paper} style={{backgroundColor:'blue'}}>Death</Paper></Paper>
