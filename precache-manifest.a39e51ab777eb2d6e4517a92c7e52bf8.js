67c7ad889c56ea23c20d466a4fe13b2db9177258
MrFarhan <muhammadfarhan012345@gmail.com>
2021-09-27 14:35:52 +0500

    Updates

---
+++ precache-manifest.a39e51ab777eb2d6e4517a92c7e52bf8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2c3108b50a3882b12983e1c4e8dc74a",
+    "url": "/covid-19-tracker/index.html"
+  },
+  {
+    "revision": "8f32f1a832b270dcd524",
+    "url": "/covid-19-tracker/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "8ff3173fda1fbb23da09",
+    "url": "/covid-19-tracker/static/js/2.605ce4e4.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/covid-19-tracker/static/js/2.605ce4e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f32f1a832b270dcd524",
+    "url": "/covid-19-tracker/static/js/main.16716905.chunk.js"
+  },
+  {
+    "revision": "fb403f6422f9b7442ea3",
+    "url": "/covid-19-tracker/static/js/runtime-main.960a8408.js"
+  }
+]);
